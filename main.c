d4ac0c4de8f9535e05cad5a3a0b73a50e010a7cf
hector ariel jaworsky <40776511+jaworsky@users.noreply.github.com>
2018-09-01 12:11:54 -0300

    funciones

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "math.h" // solo el punto h
+#include <string.h>
+#include <stdio_ext.h>
+
 
 // instalar linux
 // system("clear");
--- math.c
+++ math.c
@@ -1,9 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "math.h" // solo el punto h
+#include <string.h>
+#include <stdio_ext.h>
+
+/* al archivo.c hay que agregarle las lirberias
+*/
+
+
+
 int ingresarNumero (float*numero)
 {
 int retorno=-1;
 printf("Ingrese un numero: ");
 __fpurge(stdin); // siemrpe antes de pedir un valor
-scanf("%f",numero);
+scanf("%f",numero); // no va ampersand &, porque le estoy pasando un puntero
 
 }
 
