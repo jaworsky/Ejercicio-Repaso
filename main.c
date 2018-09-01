e1e37ce7e2d8d95cc9a166e6a5140b04387326b4
hector ariel jaworsky <40776511+jaworsky@users.noreply.github.com>
2018-09-01 11:57:19 -0300

    funciones de la calculo clase sabado

--- main.c
+++ main.c
@@ -1,112 +1,96 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "math.h" // solo el punto h
+
+// instalar linux
+// system("clear");
+// include <stdio_exth>
+// __fpurge(stdin); para limpiar el bufer en linux
+//caso 3 calcula todas las operaciones
+//caso 4 informa
 
 /*
-1-Suma de los negativos.
-2-Suma de los positivos.
-3-Cantidad de positivos.
-4-Cantidad de negativos.
-5-Cantidad de ceros.
-6-Cantidad de números pares.
-7-Promedio de positivos.
-8-Promedios de negativos.
-9-Diferencia entre positivos y negativos, (positvos-negativos).
-10-maximo y minimo.
-funcion fflush = barre la basura del buffer del teclado.
-fflush(stdin); scanf("%c",&respuesta);
-getch
-getchar
-
+el punto h tiene el prototipo, firmas
+el punto c tiene el desarrollo, las implementaciones
+por cada biblioteca 2 archivos uno .c y uno .h
+FILE - NEW - EMPTY FILE.  math.c
+se guarda dnd esta el main
++
+new file empty file math.h
 */
+
+int ingresarNumero (float* numero);
+
 int main()
 {
 
-    int numero;
-    float sumadelospositivos=0;
-    float sumadelosnegativos=0;
-    int cantidaddepositivos=0;
-    int cantidaddenegativos=0;
-    int cantidaddeceros=0;
-    int cantidaddenumerospares=0;
-    float promediodepositivos;
-    float promediodenegativos;
-    int diferenciaentrepositivosynegativos;
-    int maximo=0;
-    int minimo=0;
-    char respuesta='s';
-    int bandera=0;
 
-//estructura do while ( nueva ) nos aseguramos que se ingrese por lo menos una vez
+
+    int flag;
+    int opcion;
+    int resultadosuma;
+    int resultadoresta;
+
+
+    int menu(int opcion);
+
     do
     {
 
-        printf("Ingrese un numero: \n");
-        scanf("%d",&numero);
 
-        if(numero>0)
-        {
-            sumadelospositivos=sumadelospositivos+numero;
-            cantidaddepositivos=cantidaddepositivos+1;
-        }
-        else if(numero<0)
-        {
-            sumadelosnegativos=sumadelosnegativos+numero;
-            cantidaddenegativos=cantidaddenegativos+1;
-        }
-        else
-        {
-            cantidaddeceros=cantidaddeceros+1;
-        }
+    printf("1. Ingresar 1er operando \n2. Ingresar 2do operando \n");
+    getchar(); // pide un caracter para continuar
+    printf("Ingrese una opcion del menu: \n");
+    scanf("%d",&opcion);
 
-        if(numero%2==0)
-        {
-            cantidaddenumerospares=cantidaddenumerospares+1;
-        }
-
-        if(bandera=0)
-        {
-            maximo=numero;
-            minimo=numero;
-            bandera=1;
-        }
-
-        if(numero>maximo)
-        {
-            maximo=numero;
-
-        }
-        if(numero<minimo)
-        {
-            minimo=numero;
-
-        }
+    switch(opcion)
+    {
+        case 1:
+        //ingresar numero();
+        ingresarNumero(&num1);
+        break;
+        case 2:
+        //ingresar numero();
+        ingresarNumero(&num2);
+        break;
 
 
 
-        printf("Desea continuar?  n=no \n");
-        fflush(stdin); // se utiliza para q limpie el bufer del teclado, sino no funciona
-        scanf("%c",&respuesta);
+        case 5:
+        //SALIR
+        flag=0;
+
+        default:
+        printf("Ingrese una opcion valida! \n");
+
     }
-    while(respuesta=='s');
 
 
-    promediodepositivos= (float) (sumadelospositivos/cantidaddepositivos);
-    promediodenegativos= (float) (sumadelosnegativos/cantidaddenegativos);
-    diferenciaentrepositivosynegativos=cantidaddepositivos-cantidaddenegativos;
 
 
-    printf("La suma de los positivos es: %.2f \n",sumadelospositivos);
-    printf("La cantidad de ceros es: %d \n",cantidaddeceros);
-    printf("La suma de los negativos es: %.2f \n",sumadelosnegativos);
-    printf("La cantidad de positivos es: %d \n",cantidaddepositivos);
-    printf("La cantidad de negativos es: %d \n",cantidaddenegativos);
-    printf("La cantidad de numeros pares es: %d \n",cantidaddenumerospares);
-    printf("El promedio de los positivos es: %f \n",promediodepositivos);
-    printf("El promedio de los negativos es: %f \n",promediodenegativos);
-    printf("La diferencia entre positivos y negativos es de: %d \n",diferenciaentrepositivosynegativos);
-    printf("El numero maximo es: %d y el minimo es %d \n",maximo,minimo);
+    __fpurge(stdin);
+    printf("\nIngrese ENTER para continuar..."); // detalle al final
+    getchar();
+    system("clear");
+
+
+
+    }while(flag !=0);
+
+
+
+
 
 
     return 0;
+}
+
+int ingresarNumero (float*numero)
+{
+int retorno=-1;
+printf("Ingrese un numero: ");
+__fpurge(stdin); // siemrpe antes de pedir un valor
+scanf("%f",numero);
 
 }
+
---
+++ math.c
@@ -0,0 +1,9 @@
+int ingresarNumero (float*numero)
+{
+int retorno=-1;
+printf("Ingrese un numero: ");
+__fpurge(stdin); // siemrpe antes de pedir un valor
+scanf("%f",numero);
+
+}
+
