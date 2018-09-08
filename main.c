6c7abb3d2025095a91acfedb85722166f11865b4
hector ariel jaworsky <40776511+jaworsky@users.noreply.github.com>
2018-09-07 21:54:55 -0300

    con la funcion andando

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#define TAM 5
+
+void mostrarVector( int x[], int tam);
 
 int main()
 {
-
 int vector[5]={0,0,0,0,0};
 char respuesta='s';
 int numero;
@@ -11,28 +14,28 @@ int indice;
 
 do
 {
-
     printf("Ingrese un numero: ");
     scanf("%d",&numero);
-
     printf("Ingrese el indice: ");
     scanf("%d",&indice);
-
     printf("desea continuar?");
     scanf("%c",&respuesta);
     fflush(stdin);
-
     vector[indice]=numero;
-
-
 
 }while(respuesta!='n');
 
-for(int i=0;i<5;i++) // que algo se repita 5 veces
-{
-    printf("\nlos numeros ingresados en orden son: %d\n",vector[i]);
-
-}
+mostrarVector(vector, TAM);
 
     return 0;
 }
+
+void mostrarVector( int x[], int tam)
+{
+    for(int i=0; i < tam; i++)
+    {
+        printf(" %d", x[i]);
+    }
+    printf("\n\n");
+}
+
