6af1807a7f4d277636d658696cd93b7a20bb7d28
hector ariel jaworsky <40776511+jaworsky@users.noreply.github.com>
2018-09-08 12:45:09 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,20 +1,102 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#define STRINGLEN 500
+#include <ctype.h>
+#define TAM 6
+
+int buscarNumero(int vec[], int tam, int valor);
+
+void mostrarVector( int x[], int tam);//funcion para mostrar el vector
+
+int buscarMayor(int vec[], int tam);//funcion para buscar el mayor
 
 int main()
 {
-    char miString[STRINGLEN];
-    strncpy(miString,"pepe",STRINGLEN); // para guardar algo en una cadena de caracteres
-    printf("%s",miString);
+int vector[TAM]={0}; // con poner un solo cero, ya inicializa los demas.
+char respuesta='s';
+int numero;
+int indice;
+int numeromayor;
+
+do
+{
+    printf("Ingrese un numero: ");
+    scanf("%d",&numero);
+    printf("Ingrese el indice: ");
+    scanf("%d",&indice);
+    while(indice<0 || indice>=TAM) // valido el indice del vector
+    {
+        printf("Ingrese un indice dentro del tamanio del vector: ");
+        scanf("%d",&indice);
+    }
 
 
-    //comparar string
-    // si da 0 es porque lo encontro
-    int comparacion=strncmp("pepe",miString,STRINGLEN);
-    printf("\n%d",comparacion);
+    printf("desea continuar?");
+    scanf("%c",&respuesta);
+    fflush(stdin);
+    vector[indice]=numero;
 
-return 0;
+}while(respuesta!='n');
+
+mostrarVector(vector, TAM);
+
+indice =  buscarNumero(vector, TAM, 34);
+
+numeromayor = buscarMayor(vector,TAM);
+
+printf("\nEl numero mayor del vector es: %d\n",numeromayor);
+
+if(indice!=-1)
+{
+    printf("\n El valor se encuentra en el indice: %d",indice);
+}
+else
+{
+    printf("\n El valor buscado, no se encuentra en el indice");
+}
+
+    return 0;
+}
+
+void mostrarVector( int x[], int tam)
+{
+    for(int i=0; i < tam; i++)
+    {
+        printf(" %d", x[i]);
+    }
+    printf("\n\n");
+}
+
+int buscarNumero(int vec[], int tam, int valor) // funcionando
+{
+ int indice=-1;
+
+ for(int i=0;i<tam;i++)
+ {
+     if(vec[i]==valor)
+     {
+         indice=i;
+         break;
+     }
+
+ }
+    return indice;
+
+}
+
+int buscarMayor(int vec[], int tam)
+{
+
+int mayor=vec[0]; // el mayor es el primer indice del vector.
+
+for(int i=1;i<tam;i++)
+{
+    if(vec[i]>mayor)
+    {
+        mayor=vec[i];
+    }
+
+}
+
+return mayor;
 
 }
